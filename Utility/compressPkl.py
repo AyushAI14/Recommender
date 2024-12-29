cd1634e0b07f0c94aa5eae6be7890ca8dee61e2f
AyushAI14 <ayushaiml14@gmail.com>
2024-12-29 21:22:26 +0530

    book Recommender

---
+++ Utility/compressPkl.py
@@ -0,0 +1,25 @@
+from compress_pickle import dump
+import pandas as pd
+
+# Load your original .pkl file
+df = pd.read_pickle("Model/finaldf.pkl")
+
+# Compress and save the .pkl file
+compressed_file = "finaldf_compressed.gz"  # You can choose a different compression extension (e.g., .bz2, .lzma)
+dump(df, compressed_file, compression="gzip")
+
+print(f"Compressed file saved as: {compressed_file}")
+
+
+# from compress_pickle import load
+# import pandas as pd
+
+# # Load the compressed file
+# compressed_file = "finaldf_compressed.gz"
+# df = load(compressed_file, compression="gzip")
+
+# # Save it as a standard .pkl file
+# uncompressed_file = "finaldf_uncompressed.pkl"
+# df.to_pickle(uncompressed_file)
+
+# print(f"Uncompressed file saved as: {uncompressed_file}")
---
+++ Webapp/app.py
@@ -0,0 +1,50 @@
+from flask import Flask,render_template,request
+import numpy as np
+import pickle
+import gzip
+
+finalpopularity = pickle.load(open('Model/finalpopularity.pkl', "rb"))
+
+with gzip.open('/home/ayush/Documents/AI/Machine_Learning/Projects/RecommendationYourself/Model/finaldf_compressed.gz', 'rb') as f:
+    finaldf = pickle.load(f)
+
+book = pickle.load(open('Model/book.pkl', "rb"))
+
+similarityScore = pickle.load(open('Model/similarityScore.pkl', "rb"))
+
+app = Flask(__name__,template_folder='templates',static_folder='static',static_url_path='/')
+@app.route("/")
+def start():
+    return render_template("home.html",
+                           bookName = list(finalpopularity["Book-Title"].values),
+                           Image = list(finalpopularity["Image-URL-M"].values),
+                           Isbn = list(finalpopularity["ISBN"].values),
+                           bookauthor = list(finalpopularity["Book-Author"].values),
+                           NoRating = list(finalpopularity["Book-Rating"].values),)
+@app.route("/recommend")
+def recommend():
+    return render_template("recommend.html")
+
+@app.route("/Userrecommend",methods = ["post"])
+def Userrecommend():
+    Userinput = request.form.get("Userinput")
+    index = np.where(finaldf.index == Userinput)[0][0]
+    similarBook = sorted(list(enumerate(similarityScore[index])),key = lambda x:x[1],reverse=True)[1:6]
+
+    data = []
+    for i in similarBook:
+        item = []
+        # print(finaldf.index[i[0]])
+        tempdf = book[book["Book-Title"] == finaldf.index[i[0]]]
+        item.extend(list(tempdf.drop_duplicates("Book-Title")["Book-Title"].values))
+        item.extend(list(tempdf.drop_duplicates("Book-Title")["Image-URL-M"].values))
+        item.extend(list(tempdf.drop_duplicates("Book-Title")["Book-Author"].values))
+        item.extend(list(tempdf.drop_duplicates("Book-Title")["ISBN"].values))        
+        data.append(item)
+    print(data)
+    return render_template("recommend.html", data=data,user_input=Userinput)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
